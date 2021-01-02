37bf93e0d7813c3702c7d7cd9983e4852544d38d
TristanHillyard <73565670+TristanHillyard@users.noreply.github.com>
2021-01-02 12:08:43 -0700

    Add project files.

---
+++ Structs/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Structs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Number nums = new Number();
+            nums.Amount = 3.2m;
+            Console.WriteLine(nums.Amount);
+        }
+    }
+}
